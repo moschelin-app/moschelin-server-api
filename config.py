c830e9b0192d31bd7463dbe9b74d0474b8471ed4
wonjun12 <108748094+wonjun12@users.noreply.github.com>
2023-08-22 17:14:32 +0900

    jwt 유효시간 1시간으로 변경

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Config:
     DB_HOST = 'mysqlinstance.cr2sp2mx5gjz.ap-northeast-2.rds.amazonaws.com'
     DB_USER = 'moschelin_db_user'
@@ -12,6 +14,7 @@ class Config:
     
     AWS_ACCESS_KEY_ID = 'AKIAR4D3TFNFFIUUJ56P'
     AWS_SECRET_ACCESS_KEY = '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'
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=15)
     
     S3_BUCKET = 'aws-moschelin-s3'
     S3_Base_URL = f'https://{S3_BUCKET}.s3.amazonaws.com/'
